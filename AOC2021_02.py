f08c40aa656876676fa6e084abb25cbbcb332fe5
Dennis Koopman <denniskoopman1@gmail.com>
2021-12-02 12:58:03 +0100

    Refactor day 2

--- AOC2021_02.py
+++ AOC2021_02.py
@@ -1,13 +1,17 @@
-from typing import List
+from typing import List, Tuple
 
 
-def get_input(data_file: str) -> List[List]:
-    """Read data file and return as list of integers."""
+def get_input(data_file: str) -> List[Tuple]:
+    """Read data file and return as list."""
     with open(data_file) as f:
-        return [x.strip().split() for x in f.readlines()]
+        return [tuple(x.strip().split()) for x in f.readlines()]
 
 
-def compute_p1(data: List) -> int:
+def compute_p1(data: List[Tuple]) -> int:
+    """
+    Calculate position and depth based on list of instructions.
+    Return product of position and depth. Solution for part 1.
+    """
     position = depth = 0
     for instruction in data:
         move, value = instruction
@@ -20,13 +24,17 @@ def compute_p1(data: List) -> int:
     return position * depth
 
 
-def compute_p2(data: List) -> int:
+def compute_p2(data: List[Tuple]) -> int:
+    """
+    Calculate position and depth based on list of instructions.
+    Return product of position and depth. Solution for part 2.
+    """
     aim = position = depth = 0
     for instruction in data:
         move, value = instruction
         if move == "forward":
             position += int(value)
-            depth += aim * int(value)
+            depth += int(value) * aim
         elif move == "down":
             aim += int(value)
         elif move == "up":
@@ -35,7 +43,7 @@ def compute_p2(data: List) -> int:
 
 
 if __name__ == "__main__":
-    e1 = get_input("examples/example_2021_02.txt")
+    e1 = get_input("examples/e2021_02.txt")
     assert compute_p1(e1) == 150
     assert compute_p2(e1) == 900
 
