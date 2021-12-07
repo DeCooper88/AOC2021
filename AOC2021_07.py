ba8ce13e7e248abc2e40fd818b2f66cf883600fe
Dennis Koopman <denniskoopman1@gmail.com>
2021-12-07 13:39:17 +0100

    Refactor day 7: use Gauss sum of consecutive numbers for part 2

--- AOC2021_07.py
+++ AOC2021_07.py
@@ -1,41 +1,46 @@
 from typing import List
 
 
-def get_input(data_file: str) -> List:
-    """Read data file and return as list."""
-    with open(data_file) as f:
+def get_input(crabs_file: str) -> List[int]:
+    """Read crabs file and return as list."""
+    with open(crabs_file) as f:
         return [int(x.strip()) for x in f.read().split(",")]
 
 
-def compute_p1(data: List) -> int:
-    """Solution part 1."""
-    least_fuel = sum(data) * 1000
-    for i, base in enumerate(data):
-        fuel_cost = 0
-        for pos in data:
-            fuel_cost += abs(base - pos)
-        if fuel_cost < least_fuel:
-            least_fuel = fuel_cost
-    return least_fuel
+def compute_p1(crabs: List) -> int:
+    """
+    Find the horizontal position that the crabs can align to using the least
+    fuel possible. Return the amount of fuel needed. Solution for part 1.
+    """
+    lowest_fuel = sum(crabs) * max(crabs)
+    for position in crabs:
+        fuel = 0
+        for crab in crabs:
+            fuel += abs(position - crab)
+        if fuel < lowest_fuel:
+            lowest_fuel = fuel
+    return lowest_fuel
 
 
-def compute_p2(data: List) -> int:
-    """Solution part 2."""
-    least_fuel = sum(data) * 1000
-    smallest = min(data)
-    biggest = max(data)
-    for position in range(smallest, biggest + 1):
-        fuel_cost = 0
-        for crab in data:
-            if fuel_cost > least_fuel:
+def compute_p2(crabs: List) -> int:
+    """
+    Find the horizontal position that the crabs can align to using the least
+    fuel possible. The fuel consumption increases by 1 every step. Return
+    the amount of fuel needed. The algorithm uses Gauss formula for calculating
+    the sum of consecutive numbers. Solution for part 2.
+    """
+    lowest_fuel = sum(crabs) * max(crabs)
+    start, end = min(crabs), max(crabs) + 1
+    for position in range(start, end):
+        fuel = 0
+        for crab in crabs:
+            if fuel > lowest_fuel:
                 break
-            distance = abs(crab - position)
-            start = 1
-            end = distance + 1
-            fuel_cost += sum(range(start, end))
-        if fuel_cost < least_fuel:
-            least_fuel = fuel_cost
-    return least_fuel
+            steps = abs(crab - position)
+            fuel += (steps / 2) * (1 + steps)  # Gauss sum consecutive numbers
+        if fuel < lowest_fuel:
+            lowest_fuel = fuel
+    return int(lowest_fuel)
 
 
 if __name__ == "__main__":
