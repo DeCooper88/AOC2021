2c5fda4dc077e4c6385509716214e2e59ff98b77
Dennis Koopman <denniskoopman1@gmail.com>
2021-12-09 14:13:24 +0100

    Refactor day 9

--- AOC2021_09.py
+++ AOC2021_09.py
@@ -1,11 +1,12 @@
-from typing import List, Tuple
+from typing import Deque, List, Tuple
 from collections import deque
 import math
 
-DIRECTIONS = ((0, 1), (1, 0), (0, -1), (-1, 0))
+DIRECTIONS = ((0, 1), (1, 0), (0, -1), (-1, 0))  # East, South, West, North
 
 
 def get_input(data_file: str) -> List:
+    """Read data file and return as list."""
     with open(data_file) as f:
         grid = []
         for line in f.readlines():
@@ -15,38 +16,16 @@ def get_input(data_file: str) -> List:
 
 
 class LavaCave:
-    def __init__(self, map):
-        self.map = map
-        self.east_edge = len(map[0])
-        self.south_edge = len(map)
+    def __init__(self, grid: List):
+        self.grid = grid
+        self.east_edge = len(grid[0])
+        self.south_edge = len(grid)
 
-    def lowest_neighbour(self, location: Tuple) -> int:
-        """Find neighbour with lowest height."""
-        row, col = location
-        height = self.map[row][col]
-        neighbours = []
-        for direction in DIRECTIONS:
-            ns, ew = direction
-            new_row = row + ns
-            new_col = col + ew
-            if 0 <= new_row < self.south_edge and 0 <= new_col < self.east_edge:
-                neighbours.append(self.map[new_row][new_col])
-        return height < min(neighbours)
-
-    @property
-    def low_points(self):
-        low_points = []
-        for row in range(self.south_edge):
-            for col in range(self.east_edge):
-                if self.lowest_neighbour((row, col)):
-                    low_points.append((row, col))
-        return low_points
-
-    def compute_p1(self):
-        lowest = [self.map[row][col] for row, col in self.low_points]
-        return sum(lowest) + (len(lowest) * 1)
-
-    def get_neighbours(self, location):
+    def get_neighbours(self, location: Tuple) -> List:
+        """
+        Return all neighbours that are to the east, south, north or west of
+        the location.
+        """
         neighbours = []
         row, col = location
         for direction in DIRECTIONS:
@@ -57,28 +36,59 @@ class LavaCave:
                 neighbours.append((new_row, new_col))
         return neighbours
 
-    def size_basin(self, location):
+    @property
+    def low_points(self) -> List:
+        """
+        Return all low points. These are locations that are lower than any of
+        its adjacent locations.
+        """
+        low_points = []
+        for row in range(self.south_edge):
+            for col in range(self.east_edge):
+                height = self.grid[row][col]
+                neighbours = self.get_neighbours((row, col))
+                heights = [self.grid[r][c] for r, c in neighbours]
+                if height < min(heights):
+                    low_points.append((row, col))
+        return low_points
+
+    def size_basin(self, low_point: Tuple) -> int:
+        """
+        Return size of basin. Uses breadth first search to determine the size
+        of the basin.
+        """
         basin = 0
-        frontier = deque()
-        frontier.append(location)
-        seen = set()
+        frontier: Deque = deque()
+        frontier.append(low_point)
+        seen = {low_point}
         while frontier:
-            cur_loc = frontier.popleft()
-            row, col = cur_loc
-            seen.add(cur_loc)
-            if self.map[row][col] < 9:
+            location = frontier.popleft()
+            row, col = location
+            if self.grid[row][col] < 9:
                 basin += 1
-            neighbours = self.get_neighbours(cur_loc)
+            neighbours = self.get_neighbours(location)
             for nb in neighbours:
                 if nb in seen:
                     continue
                 r, c = nb
-                if self.map[r][c] < 9:
+                if self.grid[r][c] < 9:
                     frontier.append(nb)
                     seen.add(nb)
         return basin
 
-    def compute_p2(self):
+    def compute_p1(self) -> int:
+        """
+        Return the sum of the risk levels of all low points. Risk level is
+        height + 1. Answer part 1.
+        """
+        all_lows = [self.grid[row][col] for row, col in self.low_points]
+        return sum(all_lows) + (len(all_lows) * 1)
+
+    def compute_p2(self) -> int:
+        """
+        Return the product of the sizes of the largest 3 basins. Answer
+        part 2.
+        """
         basins = []
         for low in self.low_points:
             basins.append(self.size_basin(low))
@@ -86,13 +96,13 @@ class LavaCave:
         return math.prod(largest)
 
 
-e1 = get_input("examples/e2021_09.txt")
-t1 = LavaCave(e1)
-assert t1.compute_p1() == 15
-assert t1.compute_p2() == 1134
+if __name__ == "__main__":
+    e1 = get_input("examples/e2021_09.txt")
+    t1 = LavaCave(e1)
+    assert t1.compute_p1() == 15
+    assert t1.compute_p2() == 1134
 
-day9 = get_input("inputs/2021_09.txt")
-# print(day9)
-d1 = LavaCave(day9)
-print("day 9 part 1 =", d1.compute_p1())
-print("day 9 part 2 =", d1.compute_p2())
+    day9 = get_input("inputs/2021_09.txt")
+    d1 = LavaCave(day9)
+    print("day 9 part 1 =", d1.compute_p1())
+    print("day 9 part 2 =", d1.compute_p2())
